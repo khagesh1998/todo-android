d9f949e689ea1f079841e500167ab117e2b5d4e5
khagesh1998 <khagesh.kumar@dream11.com>
2022-04-24 20:28:57 +0530

    feat: get text from events

--- app/src/main/java/com/example/android1/MainActivity.kt
+++ app/src/main/java/com/example/android1/MainActivity.kt
@@ -1,21 +1,46 @@
 package com.example.android1
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private fun hideSoftKeyboard(view: View) {
+        val manager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        manager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    private fun addTodo(view: View, input: EditText){
+        val dummy = input.text.toString()
+        Toast.makeText(this,dummy,Toast.LENGTH_LONG).show()
+        hideSoftKeyboard(view)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val input = findViewById<EditText>(R.id.todoInput)
         val button = findViewById<Button>(R.id.todoButton)
-        button.setOnClickListener {
-            val dummy = input.text.toString()
-            Toast.makeText(this,dummy,Toast.LENGTH_LONG).show()
+
+        // Click on button to add to TODO list
+        button.setOnClickListener {view->
+            addTodo(view,input)
+        }
+
+        // Enter to add to TODO list
+        input.setOnKeyListener { view, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
+                addTodo(view,input)
+            }
+            false
         }
     }
 }
