6d1eef8039f86146d891aba9aa2e47a424e9f992
khagesh1998 <khagesh.kumar@dream11.com>
2022-04-24 15:29:43 +0530

    feat: read editable text

--- app/src/main/java/com/example/android1/MainActivity.kt
+++ app/src/main/java/com/example/android1/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.android1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val input = findViewById<EditText>(R.id.todoInput)
+        val button = findViewById<Button>(R.id.todoButton)
+        button.setOnClickListener {
+            val dummy = input.text.toString()
+            Toast.makeText(this,dummy,Toast.LENGTH_LONG).show()
+        }
     }
 }
